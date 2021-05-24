9392f146e91fd7b339e372a1093ad6f3821b38ad
BenoitObelia <46625877+BenoitObelia@users.noreply.github.com>
2021-05-24 11:47:31 +0200

    Delete tri_selection.py

--- tri_selection.py
+++
@@ -1,9 +0,0 @@
-L = [2,6,4,8,9,1,7,5,15,14,85,13,70,10]
-
-for i in range(len(L)):
-    min=i
-    for j in range(i+1,len(L)):
-        if L[j]<L[min]:
-            min=j
-    L[i],L[min]=L[min],L[i]
-print(L)
